b69258382baba44f5ffc65fd1f49062d451ea842
Jonathon Arbaugh <jonathonarbaugh@gmail.com>
2021-07-25 13:29:47 -0400

    Initial commit integrating D3.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import BarChart from './BarChart';
 import './App.css';
+import * as d3 from 'd3';
 
 function App() {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setData] = useState([]);
+
+  console.log("error: ", error);
+  console.log("isLoaded: ", isLoaded);
+  console.log("data: ", data);
+
+  useEffect(() => {
+    d3.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
+      .then(
+        (result) => {
+          // console.log("Data loaded!");
+          // console.log("Result:");
+          // console.log(result);
+          // console.log(JSON.stringify(result));
+          setData(result);
+          setIsLoaded(true);
+          // console.log("Done calling hooks.");
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, []);
+
+  if (!isLoaded) {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        { error.toString() }
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BarChart data={data} />
       </header>
     </div>
   );
